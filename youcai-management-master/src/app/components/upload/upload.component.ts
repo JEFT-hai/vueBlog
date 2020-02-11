31a1cdda68e5f14d61db9f94f85deefb7c65f0d5
JEFT-hai <3433891733@qq.com>
2020-02-11 11:05:57 +0800

    重写上传商品中的上传图片功能并添加截图功能

--- youcai-management-master/src/app/components/upload/upload.component.ts
+++ youcai-management-master/src/app/components/upload/upload.component.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-02-08 00:11:13
- * @LastEditTime : 2020-02-10 02:10:50
+ * @LastEditTime : 2020-02-11 02:45:27
  * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \youcai-management-master\src\app\components\upload\upload.component.ts
@@ -11,6 +11,7 @@ import { Component , Input} from '@angular/core';
 import { UploadFile } from 'ng-zorro-antd/upload';
 import { NzMessageService } from 'ng-zorro-antd';
 import { Observable, Observer } from 'rxjs';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 
 @Component({
   selector: 'jeft-upload',
@@ -20,47 +21,53 @@ import { Observable, Observer } from 'rxjs';
 
 export class UploadComponent {
   @Input() maxLength: Number = 3;
-  @Input() ratio: String = '16: 9'
+  @Input() ratio: Number = 16 / 9;
+  imageCropperShow: Boolean = false;
+  transform: object = {};
   showUploadList = {
     showPreviewIcon: true,
     showRemoveIcon: true,
     hidePreviewIconInNonImage: true
   };
-  fileList = [
-    // {
-    //   uid: -1,
-    //   name: 'xxx.png',
-    //   status: 'done',
-    //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
-    // }
-  ];
+  scale: number = 1;
+  fileList: any = [];
   previewImage: string | undefined = '';
   previewVisible = false;
 
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+
   constructor(private msg: NzMessageService) {}
 
-  beforeUpload = (file: File) => {
-    return new Observable((observer: Observer<boolean>) => {
-      // check height
-      this.checkImageDimension(file).then(dimensionRes => {
-        if (!dimensionRes) {
-          this.msg.error(`图片比例只支持 ${this.ratio}！！！`);
-          observer.complete();
-          return;
-        }
-
-        observer.next(dimensionRes);
-        observer.complete();
-      });
-    });
+  //  base64 => file
+  dataURLtoFile (dataUrl) {
+    var arr = dataUrl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    let file = new File([u8arr], 'file', {type:mime});
+    return file
   };
 
-  private getBase64(img: File, callback: (img: string) => void): void {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => callback(reader.result!.toString()));
-    reader.readAsDataURL(img);
+  // 选择图片方法
+  async fileChangeEvent(event: any) {
+    const file = event.target.files[0]
+    const ok = await this.checkImageDimension(file)
+    if (ok) {
+      const cover = this.imageCropped
+      this.resolveImage({
+        originFile: file,
+        cover: cover
+      })
+    } else {
+      this.msg.error(`图片宽高比必须要为 ${this.ratio}`);
+      this.imageChangedEvent = event;
+      this.imageCropperShow = true
+    }
   }
 
+  // 图片上传过滤
   private checkImageDimension(file: File): Promise<boolean> {
     return new Promise(resolve => {
       const img = new Image(); // create image
@@ -69,17 +76,61 @@ export class UploadComponent {
         const width = img.naturalWidth;
         const height = img.naturalHeight;
         window.URL.revokeObjectURL(img.src!);
-        resolve(width/height === Number(this.ratio));
+        resolve(width/height === this.ratio);
       };
     });
   }
 
-  handlePreview = (file: UploadFile) => {
-    this.previewImage = file.url || file.thumbUrl;
+  resolveImage (fileItem) {
+    this.fileList.push(fileItem)
+  }
+
+  // 对于图片item的操作
+  handlePreview = (item) => {
+    this.previewImage = item.cover || item.url;
     this.previewVisible = true;
   };
+  
+  handleDelete = (item) => {
+    const index = this.fileList.findIndex(itm => item === itm);
+    this.fileList.splice(index, 1)
+  }
+  
+  // image-croper 方法
 
-  getFiles = () => {
-    console.log(this.fileList)
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
+    return this.croppedImage
+  }
+
+  zoomIn (): void {
+    this.scale -= .1;
+    this.transform = {
+        ...this.transform,
+        scale: this.scale
+    };
+  }
+  zoomOrigin (): void {
+    this.scale = 1;
+    this.transform = {
+        ...this.transform,
+        scale: this.scale
+    };
+  }
+  zoomOut (): void {
+    this.scale += .1;
+    this.transform = {
+        ...this.transform,
+        scale: this.scale
+    };
+  }
+  imageConfirm (): void {
+    const file = this.dataURLtoFile(this.croppedImage);
+    const fileItem = {
+      originFile: this.dataURLtoFile(this.croppedImage),
+      cover: this.croppedImage
+    }
+    this.resolveImage(fileItem)
+    this.imageCropperShow = false
   }
 }
--- youcai-management-master/src/app/components/upload/upload.module.ts
+++ youcai-management-master/src/app/components/upload/upload.module.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-02-08 03:31:46
- * @LastEditTime : 2020-02-10 00:12:31
+ * @LastEditTime : 2020-02-11 01:26:51
  * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \youcai-management-master\src\app\components\component.module.ts
@@ -12,11 +12,11 @@ import { CommonModule } from '@angular/common';
 import { UploadComponent } from './upload.component'
 
 import { NzUploadModule } from 'ng-zorro-antd/upload'
-import { NzIconModule, NzModalModule } from 'ng-zorro-antd'
-import { ɵAnimationGroupPlayer } from '@angular/animations';
+import { NzIconModule, NzModalModule, NzButtonModule } from 'ng-zorro-antd'
+import { ImageCropperModule } from 'ngx-image-cropper';
 
 @NgModule({
-    imports: [CommonModule, NzUploadModule, NzIconModule, NzModalModule],
+    imports: [CommonModule, NzUploadModule, NzIconModule, NzModalModule, ImageCropperModule, NzButtonModule],
     declarations: [UploadComponent],
     exports: [UploadComponent]
 })
